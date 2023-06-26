9f41596ebd95c8d8eb18e0d2e8f1b12fed59962f
Hasan-Alghafir <hasan.ghafir12@gmail.com>
2023-06-26 21:48:34 +0300

    Upload The Design

---
+++ main.js
@@ -0,0 +1,10 @@
+// let box = document.querySelector(".searching");
+// let search = document.querySelector(".form");
+// console.log(search);
+
+// search.onclick = function() {
+//     box.style.cssText = "z-index: 2; opacity: 1; position: fixed" ;
+//     if(box.style.opacity === 1) {
+//         box.style.cssText = "z-index: -2; opacity: 0" ;
+//     }
+// }
